7b8f6eacf330d9c31c4f9b615d402941e9d9a0ef
root <root@d3a2e0704417>
2017-06-08 11:15:16 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_tCSafMgfbw",
+  "homePage" : "Main",
+  "name" : "Auto_tCSafMgfbw",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
